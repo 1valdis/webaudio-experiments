9dcade58c217adc4b7687c4c9b10049b38328fdf
1valdis <vladhuba@gmail.com>
2019-05-11 18:33:39 +0300

    simple playing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends PureComponent {
+  componentDidMount = () => {
+    this.context = new AudioContext();
+  }
+
+  render = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" id="input" onChange={this.openFile}/>
+      <button onClick={this.play}>Проиграй это</button>
     </div>
   );
+
+  openFile = (e) => {
+    const reader = new FileReader()
+    reader.onload = async (e) => {
+      this.source = this.context.createBufferSource()
+      this.source.buffer = await this.context.decodeAudioData(e.target.result)
+      this.source.connect(this.context.destination)
+      console.log('done')
+    }
+    reader.readAsArrayBuffer(e.target.files[0])
+  }
+
+  play = async () => {
+    console.log(this.context.state)
+    if (this.context.state === 'suspended') {
+      await this.context.resume()
+    }
+    this.source.start(0)
+    console.log('done')
+  }
 }
 
 export default App;
