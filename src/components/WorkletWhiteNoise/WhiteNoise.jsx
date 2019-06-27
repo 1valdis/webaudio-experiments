d5e4be9a869ec0033f5c799330e8997bc458e86e
1valdis <vladhuba@gmail.com>
2019-06-27 12:25:37 +0300

    add custom audioparam to white noise node

--- src/components/WorkletWhiteNoise/WhiteNoise.jsx
+++ src/components/WorkletWhiteNoise/WhiteNoise.jsx
@@ -1,29 +1,29 @@
 import React, { PureComponent } from 'react'
 
-class WhiteNoizeWorkletNode extends window.AudioWorkletNode {
-  constructor (context) {
-    super(context, 'white-noise-processor')
-  }
-}
-
 export default class WhiteNoize extends PureComponent {
   constructor (...args) {
     super(...args)
     this.state = {
       initializing: true
     }
+  }
+  componentDidMount () {
     this.init()
   }
   async init () {
     this.audioContext = new window.AudioContext()
     await this.audioContext.audioWorklet.addModule('white-noise-processor.js')
-    this.whiteNoizeNode = new WhiteNoizeWorkletNode(this.audioContext)
-    this.whiteNoizeNode.connect(this.audioContext.destination)
+    this.whiteNoiseNode = new window.AudioWorkletNode(this.audioContext, 'white-noise-processor')
+    this.whiteNoiseNode.connect(this.audioContext.destination)
     this.setState({
       initializing: false
     })
+    console.log(this.whiteNoiseNode.parameters)
   }
   render () {
-    return <button onClick={() => this.audioContext.resume()} disabled={!this.state.initializing}>play</button>
+    return <div>
+      <button onClick={() => this.audioContext.resume()} disabled={!this.state.initializing}>play</button>
+      <input type='range' onChange={(e) => this.whiteNoiseNode.parameters.get('customGain').linearRampToValueAtTime(+e.target.value, this.audioContext.currentTime + 0.5)} min='0' max='1' step='0.01' />
+    </div>
   }
 }
