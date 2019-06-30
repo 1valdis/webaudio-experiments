4856a0cf702ac5c8568e1c0964f006f75160b427
1valdis <vladhuba@gmail.com>
2019-06-30 21:03:07 +0300

    playing with biquadfilter

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,70 @@ import React, { Component } from 'react'
 import './App.css'
 
 // import FrequencyGraph from './components/FrequencyGraph/FrequencyGraph'
-// import LiveFrequencyGraph from './components/LiveFrequencyGraph/LiveFrequencyGraph'
+import SingleChannelLiveFrequencyGraph from './components/SingleChannelLiveFrequencyGraph/SingleChannelLiveFrequencyGraph'
 // import Player from './components/Player/Player'
 import WorkletWhiteNoise from './components/WorkletWhiteNoise/WhiteNoise'
 
 class App extends Component {
+  constructor (...args) {
+    super(...args)
+    this.audioContext = new AudioContext()
+    this.filter = this.audioContext.createBiquadFilter()
+    this.state = {
+      working: false,
+      audioBuffer: null
+    }
+  }
+
   render () {
     return <div className='App'>
       {/* <input type="file" id="input" accept="audio/*" onChange={this.openFile}/>
       <button onClick={this.play} disabled={this.state.working || !this.source}>Play</button>
       <FrequencyGraph buffer={this.state.audioBuffer}></FrequencyGraph>
-      <Player />
-      <LiveFrequencyGraph audioContext={this.audioContext} fromNode={this.source}></LiveFrequencyGraph> */}
-      <WorkletWhiteNoise />
+      <WorkletWhiteNoise />*/}
+      <input type="file" id="input" accept="audio/*" onChange={e => this.openFile(e)}/>
+      <input type='range' id="frequency" onChange={(e) => {this.filter.frequency.value = +e.target.value; this.forceUpdate()}} min='0' max='10000' step='1' value={this.filter.frequency.value} />
+      <input type='range' id="detune" onChange={(e) => {this.filter.detune.value = +e.target.value; this.forceUpdate()}} min='0' max='10000' step='1' value={this.filter.detune.value} />
+      <input type='range' id="gain" onChange={(e) => {this.filter.gain.value = +e.target.value; this.forceUpdate()}} min='-50' max='50' step='0.1' value={this.filter.gain.value} />
+      <input type='range' id="Q" onChange={(e) => {this.filter.Q.value = +e.target.value; this.forceUpdate()}} min='0.001' max='10' step='0.001' value={this.filter.Q.value}/>
+      <select onChange={(e) => {this.filter.type = e.target.value; this.forceUpdate()}} value={this.filter.type}>
+        <option>lowpass</option>
+        <option>highpass</option>
+        <option>bandpass</option>
+        <option>lowshelf</option>
+        <option>highshelf</option>
+        <option>peaking</option>
+        <option>notch</option>
+        <option>allpass</option>
+      </select>
+      <SingleChannelLiveFrequencyGraph audioContext={this.audioContext} fromNode={this.filter}></SingleChannelLiveFrequencyGraph>
     </div>
+  }
+
+  openFile (e) {
+    if (!e.target.files.length) return
+    this.setState({ working: true })
+    const reader = new window.FileReader()
+    reader.onload = async (e) => {
+      const buffer = await this.audioContext.decodeAudioData(e.target.result)
+      this.source = this.audioContext.createBufferSource()
+      this.source.buffer = buffer
+      this.source.connect(this.filter)
+      this.filter.connect(this.audioContext.destination)
+      this.play()
+      this.setState({ working: false })
+    }
+    reader.readAsArrayBuffer(e.target.files[0])
+  }
+
+  async play () {
+    console.log(this.audioContext.state)
+    if (this.audioContext.state === 'suspended') {
+      await this.audioContext.resume()
+      console.log('resumed')
+    }
+    this.source.start(0)
+    console.log('started')
   }
 }
 
---
+++ src/components/SingleChannelLiveFrequencyGraph/SingleChannelLiveFrequencyGraph.jsx
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react'
+import gradient from '../gradient.json'
+
+export default class FrequencyGraph extends PureComponent {
+  constructor(...args) {
+    super(...args)
+    this.state = {
+      working: true,
+    }
+  }
+  async componentDidMount() {
+    this.canvas.width = document.body.clientWidth;
+    this.canvas.height = 512;
+    this.ctx = this.canvas.getContext('2d');
+    this.analyzerNode = this.props.audioContext.createAnalyser()
+    this.analyzerNode.fftSize = this.canvas.height * 2
+    this.analyzerNode.smoothingTimeConstant = 0
+    this.props.fromNode.connect(this.analyzerNode)
+    this.animate()
+    this.setState({animating: true})
+  }
+  animate() {
+    requestAnimationFrame(this.animate.bind(this))
+    const frequencyData = new Uint8Array(this.analyzerNode.frequencyBinCount);
+    this.analyzerNode.getByteFrequencyData(frequencyData)
+    const canvasWidth = this.canvas.width
+    const canvasHeight = this.canvas.height
+    const imageData = this.ctx.getImageData(canvasWidth/2-1, 0, 1, canvasHeight)
+
+    const buf = new ArrayBuffer(imageData.data.length)
+    const buf8 = new Uint8ClampedArray(buf)
+    const data = new Uint32Array(buf)
+    for (let y = 0; y < canvasHeight; y++) {
+      const value = frequencyData[y]
+        data[canvasHeight - y] =
+          (255 << 24) |    // alpha
+          (gradient[value][2] << 16) |    // blue
+          (gradient[value][1] << 8) |    // green
+          gradient[value][0];            // red
+    }
+    imageData.data.set(buf8)
+    this.ctx.drawImage(this.ctx.canvas, -1, 0)
+    this.ctx.putImageData(imageData, this.canvas.width/2 - 1, 0)
+  }
+  render() {
+    return <canvas
+      style={{ background: 'black', display: 'block' }}
+      ref={(ref) => this.canvas = ref}>
+    </canvas>
+  }
+}
